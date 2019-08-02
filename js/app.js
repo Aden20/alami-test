b9d02dae4908e0c545f2f9e6a61a97ca0ab4117a
Aden Rahmandi <24493562+Aden20@users.noreply.github.com>
2019-08-02 09:56:39 +0700

    Adding Variable for DRY
    Adding Variabel wrapperGallery in JavaScript for use in another method

--- js/app.js
+++ js/app.js
@@ -1,29 +1,32 @@
 $(document).ready(function () {
     if ($(window).width() > 992) {
+
+        var $wrapperGallery = $(".item-gallery")
+
         $("#gridView").on("click", function () {
-            if ($(".item-gallery").hasClass("gallery-view")) {
-                $(".item-gallery").removeClass("gallery-view");
-                $(".item-gallery").addClass("grid-view");
+            if ($wrapperGallery.hasClass("gallery-view")) {
+                $wrapperGallery.removeClass("gallery-view");
+                $wrapperGallery.addClass("grid-view");
             }
         });
         $("#galleryView").on("click", function () {
-            if ($(".item-gallery").hasClass("grid-view")) {
-                $(".item-gallery").removeClass("grid-view");
-                $(".item-gallery").addClass("gallery-view");
+            if ($wrapperGallery.hasClass("grid-view")) {
+                $wrapperGallery.removeClass("grid-view");
+                $wrapperGallery.addClass("gallery-view");
             }
         });
     }
     $("#fullView").on("click", function () {
-        if ($(".item-gallery").hasClass("gallery-view")) {
-            $(".item-gallery").removeClass("gallery-view");
-            $(".item-gallery").addClass("full-view");
+        if ($wrapperGallery.hasClass("gallery-view")) {
+            $wrapperGallery.removeClass("gallery-view");
+            $wrapperGallery.addClass("full-view");
             $(".hide-mobile").css("display", "block");
         }
     });
     $("#galleryView").on("click", function () {
-        if ($(".item-gallery").hasClass("full-view")) {
-            $(".item-gallery").removeClass("full-view");
-            $(".item-gallery").addClass("gallery-view");
+        if ($wrapperGallery.hasClass("full-view")) {
+            $wrapperGallery.removeClass("full-view");
+            $wrapperGallery.addClass("gallery-view");
             $(".hide-mobile").css("display", "none");
         }
     });
@@ -31,7 +34,7 @@ $(document).ready(function () {
     $(".item-button .item-menu").on("click", function () {
         $(".item-button .item-menu.active").removeClass("active");
         $(this).addClass("active");
-        if ($(".item-gallery").hasClass("grid-view")) {
+        if ($wrapperGallery.hasClass("grid-view")) {
             $(".item-gallery .card-artikel").css("display", "flex");
             $(".item-gallery .container-data").css("alignSelf", "center");
         } else {
@@ -39,7 +42,7 @@ $(document).ready(function () {
             $(".item-gallery .container-data").css("alignSelf", "normal");
         }
         if ($(window).width() < 992) {
-            if ($(".item-gallery").hasClass("full-view")) {
+            if ($wrapperGallery.hasClass("full-view")) {
                 $(".item-gallery .card-artikel").removeClass("mobile-show");
             } else {
                 $(".item-gallery .card-artikel").addClass("mobile-show");
@@ -111,9 +114,9 @@ $(document).ready(function () {
                 $(".current-date").html(namaBulan);
 
                 if ($(window).width() < 992) {
-                    $(".item-gallery").append('<div class="card-artikel mobile-show"><img src=" ' + gallery[i]["thumbnailUrl"] + ' " alt="' + gallery[i]["title"] + '"><div class="container-data"><h2>' + gallery[i]["title"] + '</h2><p class="hide-mobile">' + gallery[i]["title"] + '<a href="' + gallery[i]["url"] + '" target="_blank" class="hide-mobile"> read more</a></p><h6 class="hide-mobile">1 july 2019</h6></div></div>');
+                    $wrapperGallery.append('<div class="card-artikel mobile-show"><img src=" ' + gallery[i]["thumbnailUrl"] + ' " alt="' + gallery[i]["title"] + '"><div class="container-data"><h2>' + gallery[i]["title"] + '</h2><p class="hide-mobile">' + gallery[i]["title"] + '<a href="' + gallery[i]["url"] + '" target="_blank" class="hide-mobile"> read more</a></p><h6 class="hide-mobile">1 july 2019</h6></div></div>');
                 } else {
-                    $(".item-gallery").append('<div class="card-artikel"><img src=" ' + gallery[i]["thumbnailUrl"] + ' " alt="' + gallery[i]["title"] + '"><div class="container-data"><h2>' + gallery[i]["title"] + '</h2><p>' + gallery[i]["title"] + '<a href="' + gallery[i]["url"] + '" target="_blank"> read more</a></p><h6>1 july 2019</h6></div></div>');
+                    $wrapperGallery.append('<div class="card-artikel"><img src=" ' + gallery[i]["thumbnailUrl"] + ' " alt="' + gallery[i]["title"] + '"><div class="container-data"><h2>' + gallery[i]["title"] + '</h2><p>' + gallery[i]["title"] + '<a href="' + gallery[i]["url"] + '" target="_blank"> read more</a></p><h6>1 july 2019</h6></div></div>');
                 }
                 return i < galleryTotal;
             });
